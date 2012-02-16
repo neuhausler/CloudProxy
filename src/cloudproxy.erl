76fab46c155359ccccaa9eeaa8238e7ef6ca971d
Marcel Neuhausler <neuhausler@gmail.com>
2012-02-15 22:29:50 -0500

    Work in progress

--- src/cloudproxy.erl
+++ src/cloudproxy.erl
@@ -1,48 +1,59 @@
-%% @author author <author@example.com>
-%% @copyright YYYY author.
+%% @author Marcel Neuhausler
+%% @copyright 2012 Marcel Neuhausler
+%%
+%%    Licensed under the Apache License, Version 2.0 (the "License");
+%%    you may not use this file except in compliance with the License.
+%%    You may obtain a copy of the License at
+%%
+%%        http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%    Unless required by applicable law or agreed to in writing, software
+%%    distributed under the License is distributed on an "AS IS" BASIS,
+%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%    See the License for the specific language governing permissions and
+%%    limitations under the License.
+
 
 %% @doc cloudproxy startup code
 
 -module(cloudproxy).
--author('author <author@example.com>').
+-author('Marcel Neuhausler').
 -export([start/0, start_link/0, stop/0]).
 
 ensure_started(App) ->
-    case application:start(App) of
-        ok ->
-            ok;
-        {error, {already_started, App}} ->
-            ok
-    end.
+	case application:start(App) of
+		ok ->
+			ok;
+		{error, {already_started, App}} ->
+			ok
+	end.
 
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the app for inclusion in a supervisor tree
 start_link() ->
-    ensure_started(inets),
-    ensure_started(crypto),
-    ensure_started(mochiweb),
-    application:set_env(webmachine, webmachine_logger_module, 
-                        webmachine_logger),
+	ensure_started(inets),
+	ensure_started(crypto),
+	ensure_started(mochiweb),
+	application:set_env(webmachine, webmachine_logger_module, webmachine_logger),
     ensure_started(webmachine),
     cloudproxy_sup:start_link().
 
 %% @spec start() -> ok
 %% @doc Start the cloudproxy server.
 start() ->
-    ensure_started(inets),
-    ensure_started(crypto),
-    ensure_started(mochiweb),
-    application:set_env(webmachine, webmachine_logger_module, 
-                        webmachine_logger),
-    ensure_started(webmachine),
-    application:start(cloudproxy).
+	ensure_started(inets),
+	ensure_started(crypto),
+	ensure_started(mochiweb),
+	application:set_env(webmachine, webmachine_logger_module, webmachine_logger),
+	ensure_started(webmachine),
+	application:start(cloudproxy).
 
 %% @spec stop() -> ok
 %% @doc Stop the cloudproxy server.
 stop() ->
-    Res = application:stop(cloudproxy),
-    application:stop(webmachine),
-    application:stop(mochiweb),
-    application:stop(crypto),
-    application:stop(inets),
-    Res.
+	Res = application:stop(cloudproxy),
+	application:stop(webmachine),
+	application:stop(mochiweb),
+	application:stop(crypto),
+	application:stop(inets),
+	Res.
--- src/cloudproxy_sup.erl
+++ src/cloudproxy_sup.erl
@@ -1,10 +1,23 @@
-%% @author author <author@example.com>
-%% @copyright YYYY author.
+%% @author Marcel Neuhausler
+%% @copyright 2012 Marcel Neuhausler
+%%
+%%    Licensed under the Apache License, Version 2.0 (the "License");
+%%    you may not use this file except in compliance with the License.
+%%    You may obtain a copy of the License at
+%%
+%%        http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%    Unless required by applicable law or agreed to in writing, software
+%%    distributed under the License is distributed on an "AS IS" BASIS,
+%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%    See the License for the specific language governing permissions and
+%%    limitations under the License.
+
 
 %% @doc Supervisor for the cloudproxy application.
 
 -module(cloudproxy_sup).
--author('author <author@example.com>').
+-author('Marcel Neuhausler').
 
 -behaviour(supervisor).
 
@@ -17,41 +30,69 @@
 %% @spec start_link() -> ServerRet
 %% @doc API for starting the supervisor.
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% @spec upgrade() -> ok
 %% @doc Add processes if necessary.
 upgrade() ->
-    {ok, {_, Specs}} = init([]),
+	{ok, {_, Specs}} = init([]),
 
-    Old = sets:from_list(
-            [Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
-    New = sets:from_list([Name || {Name, _, _, _, _, _} <- Specs]),
-    Kill = sets:subtract(Old, New),
+	Old = sets:from_list([Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
+	New = sets:from_list([Name || {Name, _, _, _, _, _} <- Specs]),
+	Kill = sets:subtract(Old, New),
 
-    sets:fold(fun (Id, ok) ->
-                      supervisor:terminate_child(?MODULE, Id),
-                      supervisor:delete_child(?MODULE, Id),
-                      ok
-              end, ok, Kill),
+	sets:fold(
+		fun (Id, ok) ->
+			supervisor:terminate_child(?MODULE, Id),
+			supervisor:delete_child(?MODULE, Id),
+			ok
+		end,
+		ok,
+		Kill),
 
-    [supervisor:start_child(?MODULE, Spec) || Spec <- Specs],
-    ok.
+	[supervisor:start_child(?MODULE, Spec) || Spec <- Specs],
+	ok.
 
 %% @spec init([]) -> SupervisorTree
 %% @doc supervisor callback.
 init([]) ->
-    Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
-    {ok, Dispatch} = file:consult(filename:join(
-                         [filename:dirname(code:which(?MODULE)),
-                          "..", "priv", "dispatch.conf"])),
-    WebConfig = [
-                 {ip, Ip},
-                 {port, 8000},
-                 {log_dir, "priv/log"},
-                 {dispatch, Dispatch}],
-    Web = {webmachine_mochiweb,
-           {webmachine_mochiweb, start, [WebConfig]},
-           permanent, 5000, worker, [mochiweb_socket_server]},
-    Processes = [Web],
-    {ok, { {one_for_one, 10, 10}, Processes} }.
+	{ok, Dispatch} = file:consult(filename:join([filename:dirname(code:which(?MODULE)), "..", "priv", "dispatch.conf"])),
+	{ok, Config}   = file:consult(filename:join([filename:dirname(code:which(?MODULE)), "..", "priv", "cloudproxy.conf"])),
+	{ok, Port}     = get_option(port, Config),
+	{ok, LogDir}   = get_option(log_dir, Config),
+	{ok, PidFile}  = get_option(pid_file, Config),
+
+	filelib:ensure_dir(LogDir),
+
+	ok = file:write_file(PidFile, os:getpid()),
+
+	WebConfig = [
+		{ip, "0.0.0.0"},
+		{port, Port},
+		{log_dir, LogDir},
+		{dispatch, Dispatch},
+		{error_handler, cloudrover_error_handler}
+	],
+
+	WebServer =
+	{
+		webmachine_mochiweb,
+		{webmachine_mochiweb, start, [WebConfig]},
+		permanent,
+		5000,
+		worker,
+		[mochiweb_socket_server, cloudrover_stateserver]
+	},
+
+	Processes = [WebServer],
+	{ok, { {one_for_all, 10, 10}, Processes} }.
+
+
+%% Utils
+
+get_option(Option, Options) ->
+	case lists:keytake(Option, 1, Options) of
+		false -> {ok, foo};
+		{value, {Option, Value}, _NewOptions} -> {ok, Value}
+	end.
+
